e6d030371ead89bcc265c5afd887274ca0de68f0
Zhenya <evgeegai@gmail.com>
2023-07-09 20:47:19 +0300

    feat: establish message exchange

--- server/handlers/router.ts
+++ server/handlers/router.ts
@@ -1,3 +1,13 @@
 export function onConnect(wsClient: WebSocket) {
-  wsClient.send('message from server');
+  const initialConnection = JSON.stringify({
+    type: 'initial_connection',
+    data: JSON.stringify({}),
+  });
+
+  wsClient.send(initialConnection);
+
+  wsClient.onmessage = (event: MessageEvent<string>) => {
+    const message = JSON.parse(event.data);
+    console.log(console.log('message from client - ', message));
+  };
 }
